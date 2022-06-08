b9aad8cf44d979edcabc14ac7ec3a661e5dfe96d
Caleb Diehl <caleb.diehl@ibigroup.com>
2022-06-08 09:09:42 -0700

    refactor to individual polylines to attach data

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,40 +1,41 @@
-import { MapContainer, TileLayer, GeoJSON, Polyline } from "react-leaflet";
+import { MapContainer, TileLayer, Polyline } from "react-leaflet";
 import classes from "./Map.module.css";
 import "leaflet/dist/leaflet.css";
-import L from "leaflet";
+import "leaflet-polylineoffset";
 
 const Map = (props) => {
   const mapCenter = [42.3601, -71.0589];
 
-  const coords = [];
-
-  props.data.features.forEach((feature) => {
-    const coordList = feature.geometry.coordinates.map((pair) => {
+  const lines = props.data.features.map((feat) => {
+    //reverse coords for polyline
+    const coordList = feat.geometry.coordinates.map((pair) => {
       return [pair[1], pair[0]];
     });
 
-    coords.push(coordList);
-  });
+    const IB = feat.properties.dir_id === "Inbound";
 
-  console.log(coords);
-  //style segments
-  function styleSegments(feature) {
-    return {
-      stroke: true,
-      opacity: 0.5,
-      color:
-        feature.properties.dir_id === "Inbound" ? "darkorange" : "steelblue",
+    const options = {
+      color: IB ? "darkorange" : "steelblue",
+      offset: IB ? 5 : 0,
     };
-  }
+
+    return (
+      <Polyline
+        key={Math.random()}
+        positions={coordList}
+        pathOptions={options}
+      />
+    );
+  });
 
   return (
     <div className={classes.map}>
-      <MapContainer center={mapCenter} zoom={13}>
+      <MapContainer center={mapCenter} zoom={13} minZoom={10} maxZoom={15}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaWJpLXRyYW5zaXQtZGF0YS10ZWFtIiwiYSI6ImNrcDI4aHFzMzFpMmcydnF3OHd5N3Z0OW8ifQ.IwReYu0rGZko64sy2mbPSg"
         />
-        {coords && <Polyline positions={coords} />}
+        {lines}
       </MapContainer>
     </div>
   );
