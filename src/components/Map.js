721881b9b17199b8a097fae80d0807819977d296
Caleb Diehl <caleb.diehl@ibigroup.com>
2022-06-07 17:38:46 -0700

    coverted geojson to polyline to use offset

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,10 +1,22 @@
-import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
+import { MapContainer, TileLayer, GeoJSON, Polyline } from "react-leaflet";
 import classes from "./Map.module.css";
 import "leaflet/dist/leaflet.css";
+import L from "leaflet";
 
 const Map = (props) => {
   const mapCenter = [42.3601, -71.0589];
 
+  const coords = [];
+
+  props.data.features.forEach((feature) => {
+    const coordList = feature.geometry.coordinates.map((pair) => {
+      return [pair[1], pair[0]];
+    });
+
+    coords.push(coordList);
+  });
+
+  console.log(coords);
   //style segments
   function styleSegments(feature) {
     return {
@@ -22,7 +34,7 @@ const Map = (props) => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaWJpLXRyYW5zaXQtZGF0YS10ZWFtIiwiYSI6ImNrcDI4aHFzMzFpMmcydnF3OHd5N3Z0OW8ifQ.IwReYu0rGZko64sy2mbPSg"
         />
-        <GeoJSON style={styleSegments} data={props.data} />
+        {coords && <Polyline positions={coords} />}
       </MapContainer>
     </div>
   );
