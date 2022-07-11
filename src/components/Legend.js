882f6f0a6de13f0134c718c216a06e871ad773bb
Caleb Diehl <caleb.diehl@ibigroup.com>
2022-07-11 11:31:14 -0700

    made legend collapsable

--- src/components/Legend.js
+++ src/components/Legend.js
@@ -1,45 +1,59 @@
 import classes from "./Legend.module.css";
 import { grades } from "../globals";
+import { useState } from "react";
 
 const Legend = (props) => {
+  const [collapsed, setCollapsed] = useState(true);
   return (
     <div className={classes.container}>
-      <div className={classes["inner-container"]}>
-        <svg height="20" width="60">
-          <circle
-            cx="30"
-            cy="10"
-            r="5"
-            fill="white"
-            stroke="lightgrey"
-          ></circle>
-        </svg>
-        <p>Transit Stop</p>
-      </div>
-      <div className={classes["inner-container"]}>
-        <svg height="50" width="60">
-          <line
-            x1="10"
-            x2="50"
-            y1="10"
-            y2="10"
-            stroke="white"
-            strokeWidth="3"
-            className={classes["solid-line"]}
-          ></line>
-          <line
-            x1="10"
-            x2="50"
-            y1="30"
-            y2="30"
-            stroke="white"
-            strokeDasharray="5px 3px"
-            strokeWidth="3"
-            className={classes["dashed-line"]}
-          ></line>
-        </svg>
-        <p>Line type denotes each side of the street</p>
-      </div>
+      <button
+        className={classes["expand-btn"]}
+        onClick={() => setCollapsed((state) => !state)}
+      >
+        <span class="material-symbols-outlined">
+          {collapsed ? "open_in_full" : "close_fullscreen"}
+        </span>
+      </button>
+      {!collapsed && (
+        <div className={classes["inner-container"]}>
+          <svg height="20" width="60">
+            <circle
+              cx="30"
+              cy="10"
+              r="5"
+              fill="white"
+              stroke="lightgrey"
+            ></circle>
+          </svg>
+          <p>Transit Stop</p>
+        </div>
+      )}
+      {!collapsed && (
+        <div className={classes["inner-container"]}>
+          <svg height="50" width="60">
+            <line
+              x1="10"
+              x2="50"
+              y1="10"
+              y2="10"
+              stroke="white"
+              strokeWidth="3"
+              className={classes["solid-line"]}
+            ></line>
+            <line
+              x1="10"
+              x2="50"
+              y1="30"
+              y2="30"
+              stroke="white"
+              strokeDasharray="5px 3px"
+              strokeWidth="3"
+              className={classes["dashed-line"]}
+            ></line>
+          </svg>
+          <p>Line type denotes each side of the street</p>
+        </div>
+      )}
       <div>
         {props.colors.map((color, index) => {
           return (
@@ -51,7 +65,7 @@ const Legend = (props) => {
               >
                 <p className={classes.label}>{grades[index]}</p>
               </div>
-              <p>Text Description</p>
+              {!collapsed && <p>Text Description</p>}
             </div>
           );
         })}
